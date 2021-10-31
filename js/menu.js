57f66050cfde4efc78c144e6b247b78910a8820a
Chris Brooks <dreamincolorco@gmail.com>
2021-10-31 19:26:35 -0400

    Simple version of popout nav menu - lots to clean up

--- js/menu.js
+++ js/menu.js
@@ -1,17 +1,39 @@
 $(document).ready(function () {
+	// Default Menu
 	$.getJSON("js/navigation.json", function (data) {
 		console.log(data);
-		var output = "<ul>";
+		var defaultList = "<ul>";
 		$.each(data, function (key, val) {
 			$.each(val, function (nav, item) {
-				output += "<li>" + item.name + "</li>";
+				defaultList += "<li>" + '<a href="' + item.url + '">' + item.name + "</a>" + "</li>";
 				console.log(item);
 				if (item.name === "About Our Company") {
 					return false;
 				}
 			});
 		});
-		output += "</ul>";
-		$("nav").html(output);
+		defaultList += "</ul>";
+		$(".section--header__default-menu nav").html(defaultList);
+	});
+	// Popout Menu
+	$.getJSON("js/navigation.json", function (data) {
+		console.log(data);
+		var popoutList = "<ul>" + "<li>" + "<a href=' / '>" + "Home" + "</a>" + "</li>";
+		$.each(data, function (key, val) {
+			$.each(val, function (nav, item) {
+				popoutList += "<li>" + '<a href="' + item.url + '">' + item.name + "</a>" + "</li>";
+				console.log(item);
+			});
+		});
+		popoutList += "</ul>";
+		$(".section--header__popout-menu nav").html(popoutList);
+	});
+	// Show/Hide Menu
+	$(".section--header__menu-text").click(function () {
+		$(".section--header__popout-menu").show();
+	});
+	// Show/Hide Menu
+	$(".section--header__popout-menu .btn-close").click(function () {
+		$(".section--header__popout-menu").hide();
 	});
 });
